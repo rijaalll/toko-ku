b472b1024b37a9ceed1384fc18410afacb4f7079
Ahmad Rizal <rizalajipermana@gmail.com>
2025-05-14 19:46:14 +0700

    update mobile nav anim

--- front-end/app/_pages/home/component/navbar/navMobile.jsx
+++ front-end/app/_pages/home/component/navbar/navMobile.jsx
@@ -3,21 +3,28 @@ import { useState, useEffect } from "react";
 import NavButton from "@/app/_components/home/button/NavButton";
 import navData from "@/app/_data/home/navbarData.json";
 
-export default function NavMobile({height, setNavMobileOpen}) {
+export default function NavMobile({height, setNavMobileOpen, navMobileClose}) {
     const [ textOpen, setTextOpen ] = useState(false);
 
-    setTimeout(() => {
-        setTextOpen(true);
-    }, 100);
+    useEffect(()=> {
+        setTimeout(() => {
+            setTextOpen(true);
+        }, 100);
+    }, [])
 
     const closeNavMobile = () => {
         setNavMobileOpen(false);
     }
+
+    useEffect(() => {
+        navMobileClose && setTextOpen(false);
+    }, [navMobileClose]);
+
     return (
         <div className="w-[90%] h-full mt-8">
             <div className="w-full flex flex-col gap-y-3.5 items-start">
                 {navData.map((item, index) => (
-                    <div key={index} onClick={closeNavMobile} style={{transitionDelay: `${index * 0.1}s`}} className={`${textOpen ? 'translate-x-0' : '-translate-x-[200%]'} duration-300 w-auto h-auto cursor-pointer`}>
+                    <div key={index} onClick={closeNavMobile} style={{transitionDelay: `${textOpen ? `${index * 100}ms` : `${(navData.length * 100)-(index * 100)}ms`}`}} className={`${textOpen ? 'translate-x-0' : '-translate-x-[200%]'} duration-300 w-auto h-auto cursor-pointer`}>
                         <NavButton title={item.title} url={item.url} height={height} fontSize={20} />
                     </div>
                 ))}
--- front-end/app/_pages/home/component/navbar/navbar.jsx
+++ front-end/app/_pages/home/component/navbar/navbar.jsx
@@ -10,12 +10,25 @@ import LinkButtonIcon from '@/app/_components/home/button/LinkButtonIcon';
 
 export default function Navbar({ height, scrollY }) {
     const [ navMobileOpen, setNavMobileOpen ] = useState(false);
+    const [ navMobileClose, setNavMobileClose ] = useState(true);
+
+    const closeNavMobile = () => {
+        if (navMobileOpen === false) {
+            setNavMobileOpen(true);
+            setNavMobileClose(false);
+        } else {
+            setNavMobileClose(true);
+            setTimeout(() => {
+                setNavMobileOpen(false);
+            }, ((navData.length * 100) + 100));
+        }
+    }
 
     return (
-        <nav className={`${navMobileOpen && 'bg-zinc-200'} fixed w-full h-auto flex flex-col items-center top-0 left-0`}>
+        <nav className={`${navMobileOpen ? 'bg-zinc-200 h-[100dvh]' : 'h-auto'} fixed w-full flex flex-col items-center top-0 left-0 duration-300`}>
             <div className={`${navMobileOpen || scrollY > 100 ? 'bg-zinc-200/30 backdrop-blur-xs' : 'bg-transparent'} ${navMobileOpen ? 'rounded-none border-b-[1px] border-black/70' : 'border-b-[1px] border-transparent rounded-3xl'} w-[90%] flex flex-row justify-between items-center px-1 duration-300 mt-5`}>
                 <div className="md:hidden p-0">
-                    <Hamburger toggled={navMobileOpen} toggle={setNavMobileOpen} size={20} duration={0.5} />
+                    <Hamburger toggled={navMobileOpen} toggle={() => closeNavMobile()} size={20} duration={0.5} />
                 </div>
                 <div>
                     <p className='text-xl'>Panjul Shop</p>
@@ -28,8 +41,8 @@ export default function Navbar({ height, scrollY }) {
                 <LinkButtonIcon url={"/cart"} icon={"bi-cart"} size={25} />
             </div>
             {navMobileOpen ? (
-                <div className='w-full h-[100dvh] flex justify-center'>
-                    <NavMobile height={height} setNavMobileOpen={setNavMobileOpen} />
+                <div className='w-full h-auto flex justify-center'>
+                    <NavMobile height={height} setNavMobileOpen={setNavMobileOpen} navMobileClose={navMobileClose} />
                 </div>  
             ) : null}
         </nav>
