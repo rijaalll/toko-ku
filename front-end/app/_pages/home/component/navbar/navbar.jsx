e673564cf8b745303b54ef1a28fa0f2dd967546b
Ahmad Rizal <rizalajipermana@gmail.com>
2025-05-15 11:10:55 +0700

    update mobile nav

--- front-end/app/_pages/home/component/navbar/navbar.jsx
+++ front-end/app/_pages/home/component/navbar/navbar.jsx
@@ -25,20 +25,25 @@ export default function Navbar({ height, scrollY }) {
     }
 
     return (
-        <nav className={`${navMobileOpen ? 'bg-zinc-200 h-[100dvh]' : 'h-auto'} fixed w-full flex flex-col items-center top-0 left-0 duration-300`}>
-            <div className={`${navMobileOpen || scrollY > 100 ? 'bg-zinc-200/30 backdrop-blur-xs' : 'bg-transparent'} ${navMobileOpen ? 'rounded-none border-b-[1px] border-black/70' : 'border-b-[1px] border-transparent rounded-3xl'} w-[90%] flex flex-row justify-between items-center px-1 duration-300 mt-5`}>
+        <nav className={`${navMobileOpen ? 'max-md:bg-zinc-200 h-[100dvh]' : 'max-md:h-auto'} ${scrollY > 100 ? 'md:bg-zinc-200/30 md:backdrop-blur-xs md:border-b-[1px] md:border-black/30' : 'md:bg-transparent md:border-b-[1px] md:border-transparent'} md:h-auto md:py-4 fixed w-full flex flex-col items-center top-0 left-0 duration-300`}>
+            <div className={`${navMobileOpen || scrollY > 100 ? 'max-md:bg-zinc-200/30 max-md:backdrop-blur-xs' : 'max-md:bg-transparent'} ${navMobileOpen ? 'max-md:rounded-none border-b-[1px] max-mdborder-black/70' : 'max-md:order-b-[1px] max-md:border-transparent max-md:rounded-3xl'} max-md:mt-5 md:relative w-[90%] flex flex-row justify-between items-center px-1 py-[2px] duration-300`}>
                 <div className="md:hidden p-0">
                     <Hamburger toggled={navMobileOpen} toggle={() => closeNavMobile()} size={20} duration={0.5} />
                 </div>
                 <div>
-                    <p className='text-xl'>Panjul Shop</p>
+                    <p className='text-xl md:text-2xl'>Panjul Shop</p>
                 </div>
-                <div className="hidden md:flex">
+                <div className="hidden md:flex md:gap-4 md:absolute md:left-1/2 md:-translate-x-1/2">
                     {navData.map((item, index) => (
-                        <NavButton key={index} title={item.title} url={item.url} height={height} />
+                        <NavButton key={index} title={item.title} url={item.url} height={height} fontSize={20} />
                     ))}
                 </div>
-                <LinkButtonIcon url={"/cart"} icon={"bi-cart"} size={25} />
+                <div className='px-1 md:hidden'>
+                    <LinkButtonIcon url={"/cart"} icon={"bi-cart"} size={23} />
+                </div>
+                <div className='px-1 max-md:hidden'>
+                    <LinkButtonIcon url={"/cart"} icon={"bi-cart"} size={25} />
+                </div>
             </div>
             {navMobileOpen ? (
                 <div className='w-full h-auto flex justify-center'>
